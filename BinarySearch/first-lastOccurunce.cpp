b80abac9de51834200349b9c6bd930ced582c5f6
kalppatel12 <kalppatelwork.10@gmail.com>
2024-02-20 00:24:16 +0530

    firstday

---
+++ BinarySearch/first-lastOccurunce.cpp
@@ -0,0 +1,58 @@
+// Given a sorted array arr of n integers and a target value k. Write a program to find the indices of the first and the last occurrences of the target value. If the target is not found then return -1 as indices.
+
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int firstOccurrence(vector<int> &arr, int n, int k) {
+    int low = 0, high = n - 1;
+    int first = -1;
+    while (low <= high) {
+        int mid = (low + high) / 2;
+        // maybe an answer
+        if (arr[mid] == k) {
+            first = mid;
+            high = mid - 1;
+        }else if (arr[mid] < k) {
+            low = mid + 1; 
+        }else {
+            high = mid - 1;
+        }
+    }
+    return first;
+}
+
+int lastOccurrence(vector<int> &arr, int n, int k) {
+    int low = 0, high = n - 1;
+    int last = -1;
+    while (low <= high) {
+        int mid = (low + high) / 2;
+        if (arr[mid] == k) {
+            last = mid;
+
+            low = mid + 1;
+        }else if (arr[mid] < k) {
+            low = mid + 1; 
+        }else {
+            high = mid - 1;
+        }
+    }
+    return last;
+}
+
+
+pair<int, int> firstAndLastPosition(vector<int>& arr, int n, int k) {
+    int first = firstOccurrence(arr, n, k);
+    if (first == -1) return { -1, -1};
+    int last = lastOccurrence(arr, n, k);
+    return {first, last};
+}
+
+
+int main(){
+    vector<int> arr =  {2, 4, 6, 8, 8, 8, 11, 13};
+    int n = 8, k = 8;
+    pair<int, int> ans = firstAndLastPosition(arr, n, k);
+    cout << "The first and last positions are: "<< ans.first << " " << ans.second << "\n";
+    return 0;
+}
